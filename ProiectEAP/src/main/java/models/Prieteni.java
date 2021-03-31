fc8702e14c063a4960415fbc98f4b9d32aaf42a4
Viezure Costin <49548869+costi1212@users.noreply.github.com>
2021-03-31 18:09:17 +0300

    Update Prieteni.java

--- ProiectEAP/src/main/java/models/Prieteni.java
+++ ProiectEAP/src/main/java/models/Prieteni.java
@@ -1,17 +1,17 @@
 package models;
-
+//this class was created so that users can transfer money betwwent themselves, but only if they are "friends" with each other
 public class Prieteni {
-
+    //table entries
     String idPrieten;
     String Useri_idUser;
     String Nume_Utilizator;
-
+    //constructor with arguments
     public Prieteni(String idPrieten, String useri_idUser, String nume_Utilizator) {
         this.idPrieten = idPrieten;
         Useri_idUser = useri_idUser;
         Nume_Utilizator = nume_Utilizator;
     }
-
+    //getters and setters
     public String getIdPrieten() {
         return idPrieten;
     }
